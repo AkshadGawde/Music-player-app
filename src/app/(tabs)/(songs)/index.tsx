cd8dad6e7cb985ea138a692b1b7337f2cc7ed934
Akshad Gawde <gawdeakshad@gmail.com>
2024-08-06 14:17:02 +0530

    added screens

---
+++ src/app/(tabs)/(songs)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+import { defaultStyles } from "@/styles"
+
+const SongsScreen =() => {
+    return <View style={defaultStyles.container}>
+        <Text style={defaultStyles.text}>Songs Screen</Text>
+    </View>
+}
+
+
+export default SongsScreen
