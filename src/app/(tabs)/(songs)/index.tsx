234e68f6965232285655c586af7739d114688635
Akshad Gawde <gawdeakshad@gmail.com>
2024-08-10 23:43:44 +0530

    added search bar

--- src/app/(tabs)/(songs)/index.tsx
+++ src/app/(tabs)/(songs)/index.tsx
@@ -1,10 +1,17 @@
 import { ScrollView, Text, View } from "react-native"
 import { defaultStyles } from "@/styles"
+import { screenPadding } from "@/constants/tokens"
 import { TracksList } from "@/components/TracksList"
+import { useNavigationSearch } from "@/hooks/useNavigationSearch"
 
 const SongsScreen =() => {
+
+    const search = useNavigationSearch({ searchBarOptions: { placeholder: 'Find in songs' } })
+
     return <View style={defaultStyles.container}>
-        <ScrollView>
+        <ScrollView 
+        contentInsetAdjustmentBehavior="automatic" 
+        style={{ paddingHorizontal: screenPadding.horizontal }}>
             <TracksList />
         </ScrollView>
            
--- src/components/TracksList.tsx
+++ src/components/TracksList.tsx
@@ -1,13 +1,24 @@
 import library from '@/assets/data/library.json';
-import { FlatList, FlatListProps } from 'react-native';
+import { FlatList, FlatListProps, View } from 'react-native';
 import { TrackListItemProps, TracksListItem } from './TrackListItem';
+import { utilsStyles } from '../styles/index';
 
+
+// Correctly typing the FlatList props
 export type TracksListProps = Partial<FlatListProps<unknown>>;
 
-export const TracksList = ({ ...flatListProps }: TracksListItemProps) => {
+// Correct export syntax for the divider component
+export const ItemDivider = () => (
+  <View style={{ ...utilsStyles.itemSeparator, marginVertical: 5, marginLeft: 50 }} />
+);
+
+export const TracksList = ({ ...flatListProps }: TracksListProps) => {
   return (
     <FlatList
       data={library}
+      contentContainerStyle={{ paddingTop:10, paddingBottom: 128 }}
+      ListFooterComponent={ItemDivider}
+      ItemSeparatorComponent={ItemDivider}
       renderItem={({ item: track }) => (
         <TracksListItem
           track={{
