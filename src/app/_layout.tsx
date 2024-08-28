d331c4f7098f664626e51eda16cabf3b1c446319
Akshad Gawde <gawdeakshad@gmail.com>
2024-08-28 21:55:27 +0530

    added logic for api

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,10 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Stack } from 'expo-router';
+import { useCallback } from 'react';
+import { useSetupTrackPlayer } from '@/hooks/useSetupTrackPlayer';
+import { SplashScreen } from 'expo-router';
 
-
+SplashScreen.preventAutoHideAsync();
 
 const App = () => {
+
+  const handelTrackPlayerLoaded = useCallback (() => {
+    SplashScreen.hideAsync();
+  }, []);
+
+
+  useSetupTrackPlayer({
+    onLoad: handelTrackPlayerLoaded,
+    
+  })
+
   return (
     <SafeAreaProvider>
       <RootNavigation />
